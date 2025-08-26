7b721f000cb5fbe6c0e235c1dbbb89c80194882a
malisov99 <alisovm99ru@mail.ru>
2025-08-26 19:49:42 +0300

    complete task

---
+++ lesson3/Task number 1.py
@@ -0,0 +1,8 @@
+#Task number 1
+
+view = input("Введите вид питомца: ")
+age = input("Введите возраст питомца: ")
+nickname = input("Введите кличку питомца: ")
+print(f"Это {view} по кличке {nickname}. Возраст: {age} лет")
+
+
---
+++ lesson3/Task number 2.py
@@ -0,0 +1,9 @@
+#Task number 2
+
+stage1 = input("Введите первую стадию развития человека: ") #Австралопитек
+stage2 = input("Введите вторую стадию развития человека: ") #Человек умелый (Homo habilis)
+stage3 = input("Введите третью стадию развития человека: ") #Человек прямоходящий (Homo erectus)
+stage4 = input("Введите четвертую стадию развития человека: ") #Неондерталец (Homo neanderthalensis)
+stage5 = input("Введите пятую стадию развития человека: ") #Человек разумный (Homo sapiens)
+stage6 = input("Введите шестую стадию развития человека: ") #Человек разумный разумный (Homo sapiens sapiens)
+print(stage1, stage2, stage3, stage4, stage5, stage6, sep=" => ")
